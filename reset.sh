631668d0e2be0d142a791db01ba2280682dc99ab
Kedar Bidarkar <kbidarka@redhat.com>
2012-02-06 17:06:02 +0530

    add comments to reset

--- reset.sh
+++ reset.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#Reset the rhui20 folder for another installation or a failed install
 rm  ./*.bkp answers_* network pulp1 pulp.conf rhua.conf host.sh run_distribute.sh
 #rm -rf rhui20-iso rhui20-iso.tar
 cp ./ans_dist_bkp/run_distribute.sh ./ans_dist_bkp/host.sh ./ans_dist_bkp/answers_file ./
